5f087657e869265cbc042b0b928bab27b95615c7
Adriana Jiménez <adrijimdav@gmail.com>
2026-03-26 18:51:14 -0600

    2nd day of Rosalind Problems!

---
+++ RNA.2.py
@@ -0,0 +1,11 @@
+### 2nd Day of Rosalind Problems: RNA Transcription
+
+print("Hi! Welcome to this DNA to RNA transplating program :)")
+dna_string = input("Enter a DNA string: ")
+
+rna_string = dna_string.replace("T", "U")
+
+print(f"Here's your new RNA string! {rna_string}")
+
+
+
